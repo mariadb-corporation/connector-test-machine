90bb5293eea1622f7a0771bdf6050a04790d4444
diego Dupin <diego.dupinmariadb.com>
2021-12-17 11:19:34 +0100

    update maxscale to version 2.5.17

--- launch.sh
+++ launch.sh
@@ -197,7 +197,7 @@ launch_docker () {
       export TEST_MAXSCALE_TLS_PORT=4009
       export COMPOSE_FILE=$PROJ_PATH/travis/maxscale-compose.yml
       if [ -z "$VERSION" ] ; then
-        export VERSION=2.5.12
+        export VERSION=2.5.17
       fi
       echo "building maxscale version $VERSION"
       #if [ "$DEBUG" = "1" ] ; then
