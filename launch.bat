bd2ff2e1d9574594a5cccf914221ea0b359d4752
rusher <diego.dupin@gmail.com>
2024-02-15 10:08:45 +0100

    adding wix

--- launch.bat
+++ launch.bat
@@ -39,7 +39,8 @@ if "%d%"=="" (
 
 
 choco install curl
-
+powershell Install-WindowsFeature Net-Framework-Core
+choco install wixtoolset
 
 call connector-test-machine/travis/windows-download.bat %v%
 msiexec /i server.msi INSTALLDIR=c:\projects\server SERVICENAME=mariadb ALLOWREMOTEROOTACCESS=true /qn
