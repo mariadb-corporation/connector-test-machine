43fd9d7f67ea79ed7ab7d961a2af4d1c3e6da2b3
rusher <diego.dupin@gmail.com>
2024-02-16 15:23:07 +0100

    update build to 11.4

--- travis/build/build.sh
+++ travis/build/build.sh
@@ -4,22 +4,22 @@ echo "**************************************************************************
 echo "* searching for last complete build"
 echo "**************************************************************************"
 
-wget -q -o /dev/null index.html https://buildbot.mariadb.net/archive/pack/11.3/
+wget -q -o /dev/null index.html https://buildbot.mariadb.net/archive/pack/11.4/
 grep -o ">build-[0-9]*" index.html | grep -o "[0-9]*" | tac | while read -r line ; do
 
-  curl -s --head https://buildbot.mariadb.net/archive/pack/11.3/build-$line/kvm-deb-jammy-amd64/md5sums.txt | head -n 1 | grep "HTTP/2 [23].." > /dev/null
+  curl -s --head https://buildbot.mariadb.net/archive/pack/11.4/build-$line/kvm-deb-jammy-amd64/md5sums.txt | head -n 1 | grep "HTTP/2 [23].." > /dev/null
   if [ $? = "0" ]; then
     echo "**************************************************************************"
     echo "* Processing $line"
     echo "**************************************************************************"
-    wget -q -o /dev/null -O $line.html  https://buildbot.mariadb.net/archive/pack/11.3/build-$line/kvm-deb-jammy-amd64/debs/binary/
+    wget -q -o /dev/null -O $line.html  https://buildbot.mariadb.net/archive/pack/11.4/build-$line/kvm-deb-jammy-amd64/debs/binary/
     grep -o ">[^\"]*\.deb" $line.html | grep -o "[^>]*\.deb" | while read -r file ; do
         if  [[ "$file" =~ ^mariadb-plugin.* ]] ;
         then
           echo "skipped file: $file"
         else
-          echo "download file: https://buildbot.mariadb.net/archive/pack/11.3/build-$line/kvm-deb-jammy-amd64/debs/binary/$file"
-          wget -q -o /dev/null -O $PROJ_PATH/travis/build/$file https://buildbot.mariadb.net/archive/pack/11.3/build-$line/kvm-deb-jammy-amd64/debs/binary/$file
+          echo "download file: https://buildbot.mariadb.net/archive/pack/11.4/build-$line/kvm-deb-jammy-amd64/debs/binary/$file"
+          wget -q -o /dev/null -O $PROJ_PATH/travis/build/$file https://buildbot.mariadb.net/archive/pack/11.4/build-$line/kvm-deb-jammy-amd64/debs/binary/$file
         fi
     done
     echo "DL complete"
