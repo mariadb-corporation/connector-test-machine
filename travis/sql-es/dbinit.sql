606245b903fe3d51c814c4c6e9e29f96b25ddf58
rusher <diego.dupin@gmail.com>
2024-07-23 14:45:00 +0200

    remove localhost user

--- travis/sql-es/dbinit.sql
+++ travis/sql-es/dbinit.sql
@@ -1,7 +1,7 @@
 CREATE USER IF NOT EXISTS 'boby'@'%' identified by 'heyPassW-!*20oRd';
 GRANT ALL ON *.* TO 'boby'@'%' /*M!100401 identified by 'heyPassw-!*20oRd'*/ with grant option;
-
-CREATE USER IF NOT EXISTS 'boby'@'localhost' identified by 'heyPassw-!*20oRd';
-GRANT ALL ON *.* TO 'boby'@'localhost' /*M!100401 identified by 'heyPassw-!*20oRd'*/ with grant option;
+--
+-- CREATE USER IF NOT EXISTS 'boby'@'localhost' identified by 'heyPassw-!*20oRd';
+-- GRANT ALL ON *.* TO 'boby'@'localhost' /*M!100401 identified by 'heyPassw-!*20oRd'*/ with grant option;
 
 CREATE DATABASE test2;
